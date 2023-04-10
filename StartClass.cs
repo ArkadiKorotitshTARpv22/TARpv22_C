4af411523ce76cf57652abcbff29dc025e3a957f
opilane <opilane@HP-CZC2349HT9>
2023-04-10 12:24:58 +0300

    Add project files.

---
+++ StartClass.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv22_C
+{
+    internal class StartClass
+    {
+        public static void Main(string[] args)
+        {
+            /*Random rnd = new Random();
+            string paev = "";
+            for (int i = 0; i < 10; i++)
+            {
+                int nr = rnd.Next(1, 7);
+                            Console.WriteLine(nr);
+
+                            switch(nr)
+                            {
+                                case 1:paev = "Esmaspäev";break;
+                                case 2:paev = "Teisipäev";break;
+                                case 3: paev = "Kolmapäev"; break;
+                                case 4: paev = "Neljapäev"; break;
+                                case 5: paev = "Reede"; break;
+                                case 6: paev = "Laupäev"; break;
+                                case 7: paev = "Pühapäev"; break;
+                                default:
+                                    paev = "Tundmatu päev";
+                                    break;
+                            }
+                            Console.WriteLine(paev);
+            }
+            int j = 0;
+            while (j<10)
+            {
+                Console.WriteLine(paev);
+                j++;
+            }
+            Console.WriteLine(j);
+            do
+            {
+                Console.WriteLine(paev);
+                j--;
+            } while (j != 0);
+            Console.WriteLine(j);
+            int[] arvud = new int[10];
+            for(int i=0;i<arvud.Length;i++)
+            {
+                arvud[i] = rnd.Next(-100, 100);
+            }
+            foreach(int arv in arvud)
+            {
+                Console.WriteLine(arv);
+            }
+            
+            /*Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.Clear();
+            Console.WriteLine("Tere tulemast!\nMis on sinu nimi?");
+            string eesnimi=Console.ReadLine();
+            Console.WriteLine("Tere " + eesnimi);
+            Console.WriteLine("Arv1: ");
+            //&&-and ||-or
+            if (eesnimi.ToUpper()=="JUKU")
+            {
+                Console.WriteLine("Lähme kinno!");
+                int vanus = int.Parse(Console.ReadLine());
+                if (vanus<0 || vanus>100)
+                {
+                    Console.WriteLine("Viga!");
+                }
+                else if(vanus<=6)
+                {
+                    Console.WriteLine("Tasuta pilet!");
+                }
+                else if(vanus>6 || vanus<14)
+                {
+                    Console.WriteLine("Laste pilet!");
+                }
+                else if(vanus>15 || vanus<65)
+                {
+                    Console.WriteLine("Täis pilet!");
+                }
+                else if(vanus>65)
+                {
+                    Console.WriteLine("Soodus pilet");
+                }
+            }
+            Console.ReadKey();
+            Console.Clear();*/
+            /*int arv1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Arv2: ");
+            int arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tehe: ");
+            char tehe = char.Parse(Console.ReadLine());
+            if (tehe=='+')
+            {
+                Console.WriteLine("Arvude {0} ja {1} summa võrdub {2}", arv1, arv2, arv1 + arv2);
+            }
+            else if (tehe=='-')
+            {
+                Console.WriteLine("Arvude {0} ja {1} summa võrdub {2}", arv1, arv2, arv1 - arv2);
+            }*/
+
+            
+
+
+            
+        }
+    }
+}
