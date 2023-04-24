b73603530d6e8cc16fc277c359dcc8778ed7e12a
Arkadi Korotots <arkadi.korotots@mjg.ee>
2023-04-24 12:16:26 +0300

    adsdds

---
+++ IseseisevTooArkadiKorotitshTARpv22/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Tax()
+        {
+            double t = 0.13;
+            Console.WriteLine("Write down the amount of money earned: ");
+            double m = int.Parse(Console.ReadLine());
+            double ml = m * t;
+            double totalm = m - ml;
+            Console.WriteLine(totalm+"$");
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------");
+
+            /*Console.WriteLine("Line:");
+            string line = Console.ReadLine();
+            string vowels = "aeouiy";
+            int lineTotal = line.Length;
+            int result = 0;
+            foreach (char symbol in line)
+            {
+                if (vowels.Contains(symbol.ToString()))
+                    result++;
+            }
+            var percent = ((double)result / (double)lineTotal) * 100;
+            Console.WriteLine(percent+"%");*/
+
+            Console.WriteLine("---------------------------------------------");
+
+            /*Console.Write("N = ");
+            var N = int.Parse(Console.ReadLine());
+            var a = new int[N];
+            var r = new Random();
+            var count = 0;
+            for (var i = 0; i < N; i++)
+            {
+                a[i] = r.Next(-3000,3000);
+                if (a[i] > -100 && a[i] < 100) count++;
+            }
+            Console.WriteLine("Count {0}", count);*/
+
+            Console.WriteLine("---------------------------------------------");
+
+            /*Tax();
+            Console.WriteLine("Retry? (y/n) ");
+            string ask = Console.ReadLine();
+            ask.ToLower();
+            if (ask=="y")
+            {
+                Tax();
+            }
+            else
+            {
+                Console.WriteLine("Goodbye");
+            }*/
+
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+}
